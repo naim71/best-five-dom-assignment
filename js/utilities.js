f67c7d8c0021c4f677a67bec478fdc8d9d81b5ae
naim71 <naimkhan1314@gmail.com>
2022-08-23 23:29:37 +0600

    final commit

--- js/utilities.js
+++
@@ -1,7 +0,0 @@
-// function getInputValueById(inputFieldId){
-//     const inputField = document.getElementById(inputFieldId);
-//     const inputFieldValueString = inputField.value;
-//     const inputFieldValue = parseFloat(inputFieldValueString);
-
-//     return inputFieldValueString; 
-// }
