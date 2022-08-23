8199237f869b0b75d5621afbae83fa0668f96270
naim71 <naimkhan1314@gmail.com>
2022-08-23 19:10:50 +0600

    js update

--- js/player-list.js
+++ js/player-list.js
@@ -1,1 +1,9 @@
-console.log('CLicked')
+const playerList = [];
+function addToList(element){
+    const playerName = element.parentNode.children[0].innerText;
+    playerList.push(playerName);
+    // console.log(playerList);
+    // console.log(playerList.length)
+    const numOfList = playerList.length;
+    document.getElementById("num-of-selected-players").innerText = numOfList;
+}
