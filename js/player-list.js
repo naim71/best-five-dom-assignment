6eb6f3fd8079c26678ec1796fc42bb6bce5ad3d1
naim71 <naimkhan1314@gmail.com>
2022-08-23 20:19:37 +0600

    aside card done

--- js/player-list.js
+++ js/player-list.js
@@ -1,9 +1,24 @@
 const playerList = [];
+
+function displayList(list){
+    const nameList = document.getElementById("player-name-list");
+    nameList.innerHTML= '';
+    for(let i=0; i < list.length; i++){
+        const name = list[i];
+
+        const li = document.createElement('li');
+        li.innerText = name;
+        nameList.appendChild(li);
+
+    }
+}
+
 function addToList(element){
+
     const playerName = element.parentNode.children[0].innerText;
     playerList.push(playerName);
-    // console.log(playerList);
-    // console.log(playerList.length)
     const numOfList = playerList.length;
     document.getElementById("num-of-selected-players").innerText = numOfList;
+
+    displayList(playerList);
 }
